300eda5b1666f40a0149b0f19fba6aab187e05a1
Tal Derei <talderei99@gmail.com>
2023-05-22 16:40:17 -0400

    added lifetimes

--- lifetimes/src/lib.rs
+++ lifetimes/src/lib.rs
@@ -1,5 +1,24 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+// Struct generic over lifetime 'a', tying the lifetime of the struct
+// to the lifetime of the inner slice. The struct can only live as long
+// as the inner slice is valid. 
+struct MyIterWrapper<'a, T> {
+    slice: &'a [T],
+}
+
+// Types 'a and T are generic parameters for the implementation
+impl<'a, T> Iterator for MyIterWrapper<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.slice.is_empty() {
+            return None;
+        }
+        // Retrieve first element
+        let element = self.slice.get(0);
+        // Set self.slice equal to other elements
+        self.slice = &self.slice[1..];
+        // Return first element
+        element
+    }
 }
 
 #[cfg(test)]
@@ -8,7 +27,13 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let collection = vec![1,2,3,4];
+        let wrapper = MyIterWrapper {
+            slice: &collection[..],
+        };
+        
+        for (index, elem) in wrapper.enumerate() {
+            assert_eq!(*elem, collection[index])
+        }
     }
 }
