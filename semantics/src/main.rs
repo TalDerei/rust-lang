b932521b1eb38f84c6a93bc5c27b727bc11aee2d
Tal Derei <talderei99@gmail.com>
2023-06-11 14:04:14 -0700

    description to semantic

--- semantics/src/main.rs
+++ semantics/src/main.rs
@@ -8,6 +8,9 @@ trait NotNecessarilyClone {}
 #[derive(Clone)]
 struct CloneByRc<NNC> where NNC: NotNecessarilyClone {
     // 'Rc: Rc<T>' implements clone for all T, using reference counting.
+    // It takes something that isn’t normally safe to clone and hides it 
+    // behind a reference counter so that cloning just means incrementing 
+    // the reference counter.
     nnc: Rc<NNC>,
 }
 
